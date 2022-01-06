ed08075af48af503678fdb5112fde3be66ed441d
rlysam <bellosamuelb@gmail.com>
2022-01-06 19:08:58 +0800

    list of posts to stringLiteral

--- lib/data/model/posts_data.dart
+++ lib/data/model/posts_data.dart
@@ -9,18 +9,18 @@ import 'package:flutter_cubit_bloc_tutorial/data/model/posts.dart';
 class PostsData {
   final String total_posts;
   final String total_pages;
-  final List<Post> posts;
-
+  final String posts;
   PostsData({
     required this.total_posts,
     required this.total_pages,
     required this.posts,
   });
 
+
   PostsData copyWith({
     String? total_posts,
     String? total_pages,
-    List<Post>? posts,
+    String? posts,
   }) {
     return PostsData(
       total_posts: total_posts ?? this.total_posts,
@@ -33,7 +33,7 @@ class PostsData {
     return {
       'total_posts': total_posts,
       'total_pages': total_pages,
-      'posts': posts.map((x) => x.toMap()).toList(),
+      'posts': posts,
     };
   }
 
@@ -41,30 +41,27 @@ class PostsData {
     return PostsData(
       total_posts: map['total_posts'] ?? '',
       total_pages: map['total_pages'] ?? '',
-      posts: List<Post>.from(map['posts']?.map((x) => Post.fromMap(x))),
+      posts: map['posts'] ?? '',
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory PostsData.fromJson(String source) =>
-      PostsData.fromMap(json.decode(source));
+  factory PostsData.fromJson(String source) => PostsData.fromMap(json.decode(source));
 
   @override
-  String toString() =>
-      'PostsData(total_posts: $total_posts, total_pages: $total_pages, posts: $posts)';
+  String toString() => 'PostsData(total_posts: $total_posts, total_pages: $total_pages, posts: $posts)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-
+  
     return other is PostsData &&
-        other.total_posts == total_posts &&
-        other.total_pages == total_pages &&
-        listEquals(other.posts, posts);
+      other.total_posts == total_posts &&
+      other.total_pages == total_pages &&
+      other.posts == posts;
   }
 
   @override
-  int get hashCode =>
-      total_posts.hashCode ^ total_pages.hashCode ^ posts.hashCode;
+  int get hashCode => total_posts.hashCode ^ total_pages.hashCode ^ posts.hashCode;
 }
